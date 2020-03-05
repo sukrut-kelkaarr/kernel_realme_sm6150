1948617002ee39fb43921f252bbff8b63566f82d
dev-harsh1998 <dev-harsh1998@hotmail.com>
2020-03-05 10:24:19 +0000

    drm: msm: tune onscreen fp timings
    * Fingerprint timings are slow by ~100us when the fingerprint sensor captures the fp image
    * notify_fp_press: should hold a vblank event when toggled via fod hal
    
    Signed-off-by: dev-harsh1998 <dev-harsh1998@hotmail.com>

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -708,6 +708,7 @@ int sde_connector_update_hbm(struct drm_connector *connector)
 	fingerprint_mode = sde_crtc_get_fingerprint_mode(c_conn->encoder->crtc->state);
 	if (OPPO_DISPLAY_AOD_SCENE == get_oppo_display_scene()) {
 		if (sde_crtc_get_fingerprint_pressed(c_conn->encoder->crtc->state)) {
+			fingerprint_mode = true;
 			sde_crtc_set_onscreenfinger_defer_sync(c_conn->encoder->crtc->state, true);
 		} else {
 			sde_crtc_set_onscreenfinger_defer_sync(c_conn->encoder->crtc->state, false);
@@ -784,6 +785,7 @@ int sde_connector_update_hbm(struct drm_connector *connector)
 			} else if (oppo_display_get_hbm_mode()) {
 				sde_crtc_set_onscreenfinger_defer_sync(c_conn->encoder->crtc->state, true);
 				rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_AOD_HBM_ON);
+				set_oppo_display_scene(OPPO_DISPLAY_AOD_HBM_SCENE);
 			    fingerprint_mode = true;
 			} else if(OPPO_DISPLAY_AOD_SCENE == get_oppo_display_scene()) {
 				rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_AOD_HBM_OFF);
@@ -799,6 +801,7 @@ int sde_connector_update_hbm(struct drm_connector *connector)
 				pr_err("backlight level is larger than 1023, need to restore it!\n");
 				dsi_panel_set_backlight(dsi_display->panel, dsi_display->panel->bl_config.bl_level);
 			}
+			set_oppo_display_scene(OPPO_DISPLAY_NORMAL_SCENE);
 			mutex_unlock(&dsi_display->panel->panel_lock);
 			if (rc) {
 				pr_err("failed to send DSI_CMD_HBM_OFF cmds, rc=%d\n", rc);
